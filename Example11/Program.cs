fb2b235a9b70ee1f9d6149a1c59e626d35481dd1
Renata19 <renatalsa@mail.ru>
2022-09-05 13:50:14 +0700

    11

---
+++ Example11/Program.cs
@@ -0,0 +1,27 @@
+﻿void Fillarray(int [] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void Printarray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+Fillarray(array);
+Printarray(array);
